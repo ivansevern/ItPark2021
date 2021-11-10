e6ace3381ed2752107ab3603d544b3c9f47402c5
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2021-11-10 21:42:49 +0300

    Lesson 4 add

---
+++ src/main/java/lesson4/FourthProgram.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+/*
+* Необходимо вычислить квадратный корень числа, не используя готовую реализацию метода Math.sqrt,
+* а пологаясь на простые арифметические операции.
+*  */
+public class FourthProgram {
+    public static void main(String[] args) {
+
+    }
+
+}
