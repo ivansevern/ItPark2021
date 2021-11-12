b4339404c979e0ccef88d6636affd4132da12c02
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2021-11-12 12:41:37 +0300

    Теперь считает только целочисленные корни

--- src/main/java/lesson4/FourthProgram.java
+++ src/main/java/lesson4/FourthProgram.java
@@ -11,7 +11,7 @@ public class FourthProgram {
         text();
     }
 
-    public static int kor(int n) {
+        public static int kor(int n) {
         if (n<2) return n;
         int i = 1;
         while (i * i <= n) {
@@ -24,18 +24,20 @@ public class FourthProgram {
     public static void text() {
         Scanner scanner = new Scanner(System.in);
         int sqrt;
-        do {
-            System.out.print("Введите число для расчета его корня: ");
-            while (!scanner.hasNextInt()) {
-                System.out.println("Вы ввели некоректное значение!");
+        System.out.print("Введите число для расчета его корня: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Вы ввели некоректное значение!");
                 scanner.next();
-            }
-            sqrt = scanner.nextInt();
-            if (sqrt <= 0) {
-                System.out.println("Значение не может быть 0 и меньше 0");
-                scanner.next();
-            }
-        } while (sqrt <= 0);
-        System.out.println("Корень целого числа: " + sqrt + " = " + kor(sqrt));
+        }
+        sqrt = scanner.nextInt();
+        if (sqrt <= 0) {
+            System.out.println("Значение не может быть 0 и меньше 0");
+            scanner.next();
+        } else if (kor(sqrt) * kor(sqrt) != sqrt) {
+            System.out.println("Это не целочисленный корень");
+        }
+        else {
+            System.out.println("Корень целого числа: " + sqrt + " = " + kor(sqrt));
+        }
     }
 }
