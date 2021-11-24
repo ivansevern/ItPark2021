a585ac00874f442518bdc7727f412cbae70315ef
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2021-11-24 23:35:29 +0300

    Solution lesson6 add menu

--- src/main/java/lesson6/FindFibOfMassive.java
+++ src/main/java/lesson6/FindFibOfMassive.java
@@ -1,28 +1,185 @@
 package lesson6;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
-// Еще не доделал!
+// Еще не доделал! Как записать массив Фибоначчи?
 public class FindFibOfMassive {
     public static void main(String[] args) {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        int n = bufferedReader.read();
+        Scanner scanner = new Scanner(System.in);
+        int[] fib = new int[10];
+        int[] cash = new int[5];
+
+
+//        System.arraycopy(digits, 5, cash, 0, 5);
+//        for (int j = 0; j < cash.length; j++) {
+//            System.out.println(cash[j] + " ");
+
+//        System.arraycopy(fib, 5, cash, 0, 5);
+//        for (int i = 0; i < cash.length; i++) {
+//            System.out.println(cash[i] + " ");
+//        }
+
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+            if (command == 1) {
+                System.out.print("Введите число: ");
+                findFib(fib);
+                System.out.println(" ");
+            } else if (command == 2) {
+
+            } else if (command == 3) {
+                System.out.println();
+            } else if (command == 0) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+
+    public static void findFib(int[] fib) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int N = n + 1;
+        int[] digits = new int[n];
+        int[] cash = new int[5];
+        digits[0] = 0;
+        digits[1] = 1;
+        for (int i = 2; i < digits.length; i++) {
+            digits[i] = digits[i - 1] + digits[i - 2];
+        }
+        System.arraycopy(fib, 5, cash, 0, 5);
+        for (int j = 0; j < cash.length; j++) {
+            System.out.println(cash[j] + " ");
+            System.out.print("Ряд Фибоначчи: ");
+            for (int i = 0; i < digits.length; i++) {
+                System.out.print(" ");
+                System.out.print(digits[i] + " ");
+            }
+            return;
+        }
+    }
 
-//        int[] fib = new int[5];
-//        fib[0] = 0;
-//        fib[1] = 1;
-//
-////        fib[2] = fib[0] + fib[1];
-//        for (int i = 2; i <= n; i++) {
-//            fib[2] = fib[0] + fib[1];
-//            fib[0] = fib[1];
-//            fib[1] = fib[2];
-//        }
-        System.out.println(n);
+    public static void printMenu() {
+        System.out.println(" ");
+        System.out.println("1 - Вычислить число Фибоначчи");
+        System.out.println("2 - Произвести новый расчет");
+        System.out.println("3 - Посмотреть прошлые вычисления");
+        System.out.println("0 - Выход");
     }
 }
+
+
+
+
+/*
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        double[] expenses = new double[7];
+
+        Scanner scanner = new Scanner(System.in);
+        Converter conv = new Converter(); // Создайте объект класса Converter
+
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble();
+
+        System.out.println("Сколько дней до зарплаты?");
+        int daysBeforeSalary = scanner.nextInt();
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
+                int currency = scanner.nextInt();
+                conv.convert(moneyBeforeSalary, currency); // Вызовите метод класса Converter
+            } else if (command == 2) {
+                getAdvice(moneyBeforeSalary, daysBeforeSalary);
+            } else if (command == 3) {
+                moneyBeforeSalary = saveExpense(scanner, moneyBeforeSalary, expenses);
+            } else if (command == 4) {
+                printAllExpenses(expenses);
+            } else if (command == 5) {
+                System.out.println("Самая большая сумма расходов на этой неделе составила " + findMaxExpense(expenses) + " руб.");
+            } else if (command == 0) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+
+    public static double saveExpense(Scanner scanner, double moneyBeforeSalary, double[] expenses) {
+        System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
+        int day = scanner.nextInt();
+        System.out.println("Введите размер траты:");
+        double expense = scanner.nextDouble();
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses[day - 1] = expenses[day - 1] + expense;
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    public static void printAllExpenses(double[] expenses) {
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        }
+    }
+
+    public static double findMaxExpense(double[] expenses) {
+        double maxExpense = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpense) {
+                maxExpense = expenses[i];
+            }
+        }
+        return maxExpense;
+    }
+
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Конвертировать валюту");
+        System.out.println("2 - Получить совет");
+        System.out.println("3 - Ввести трату");
+        System.out.println("4 - Показать траты за неделю");
+        System.out.println("5 - Показать самую большую сумму расходов за неделю");
+        System.out.println("0 - Выход");
+    }
+
+    public static void getAdvice(double moneyBeforeSalary, int daysBeforeSalary) {
+        if (moneyBeforeSalary < 3000) {
+            System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+        } else if (moneyBeforeSalary < 10000) {
+            if (daysBeforeSalary < 10) {
+                System.out.println("Окей, пора в Макдак!");
+            } else {
+                System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+            }
+        } else if (moneyBeforeSalary < 30000) {
+            if (daysBeforeSalary < 10) {
+                System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
+            } else {
+                System.out.println("Окей, пора в Макдак!");
+            }
+        } else {
+            if (daysBeforeSalary < 10) {
+                System.out.println("Отлично! Заказывайте крабов!");
+            } else {
+                System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
+            }
+        }
+    }
+}
+
+
+*/
