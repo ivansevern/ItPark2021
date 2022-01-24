1b3ca957b0ba923f31f81a2c2b7d6ce5f376eb5f
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-01-24 10:39:26 +0300

    solution test

--- src/test/java/lesson2/CalculatorTest.java
+++ src/test/java/lesson2/CalculatorTest.java
@@ -1,32 +1,57 @@
+
 package lesson2;
 
 import lesson2.SecondProgram;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+    private SecondProgram secondProgram;
+
+    @BeforeClass
+    public static void init() { System.out.println("Начало выполнения тестов"); }
+
+    @AfterClass
+    public static void destroy() {
+        System.out.println("Завершение выполнения тестов");
+    }
+
+    @Before
+    public void initBeforeTest() {
+        secondProgram = new SecondProgram();
+    }
+
+    @After
+    public void destroyAfterTest() {
+        secondProgram = null;
+    }
+
     @Test
     public void shouldCalculateSum() {
-        SecondProgram secondProgram = new SecondProgram();
-        Assert.assertEquals("проверка",30, secondProgram.sum(20, 10));
 
-        SecondProgram secondProgram2 = new SecondProgram();
-        Assert.assertEquals(10, secondProgram2.sum(-10, 20));
+        Assert.assertEquals("проверка",30, Sumable.sum(20, 10));
 
-        SecondProgram secondProgram3 = new SecondProgram();
-        Assert.assertEquals(10, secondProgram3.sum(0, 10));
+        Assert.assertEquals(10, Sumable.sum(-10, 20));
+
+        Assert.assertEquals(10, Sumable.sum(0, 10));
     }
     //тут выходит ошибка не знаю с чем связана
     //java: constructor SecondProgram() is already defined in class lesson2.SecondProgram
 
 
-//    @Test
-//    public void shouldSubtraction() {
-//        SecondProgram secondProgram = new SecondProgram();
-//        Assert.assertEquals(20, secondProgram.subtraction);
-//
-//        SecondProgram secondProgram2 = new SecondProgram();
-//        Assert.assertEquals(10, secondProgram2.subtraction);
-//    }
+    @Test
+    public void shouldSubtraction() {
+
+        Assert.assertEquals("проверка",10, Subctractionable.subtraction(20, 10));
+
+        Assert.assertEquals(-30, Subctractionable.subtraction(-10, 20));
+
+        Assert.assertEquals(10, Subctractionable.subtraction(10, 0));
+    }
 }
+
