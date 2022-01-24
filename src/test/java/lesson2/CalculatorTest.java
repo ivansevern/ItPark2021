5fd10c30c634e5867a0e5f14462a00f263bbcf1f
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-01-24 11:16:40 +0300

    solution test add some actions

--- src/test/java/lesson2/CalculatorTest.java
+++ src/test/java/lesson2/CalculatorTest.java
@@ -34,7 +34,7 @@ public class CalculatorTest {
     @Test
     public void shouldCalculateSum() {
 
-        Assert.assertEquals("проверка",30, Sumable.sum(20, 10));
+        Assert.assertEquals("проверка сложения",30, Sumable.sum(20, 10));
 
         Assert.assertEquals(10, Sumable.sum(-10, 20));
 
@@ -45,13 +45,38 @@ public class CalculatorTest {
 
 
     @Test
-    public void shouldSubtraction() {
+    public void shouldSubtractionSub() {
 
-        Assert.assertEquals("проверка",10, Subctractionable.subtraction(20, 10));
+        Assert.assertEquals("проверка вычитания",10, Subctractionable.sub(20, 10));
 
-        Assert.assertEquals(-30, Subctractionable.subtraction(-10, 20));
+        Assert.assertEquals(-30, Subctractionable.sub(-10, 20));
 
-        Assert.assertEquals(10, Subctractionable.subtraction(10, 0));
+        Assert.assertEquals(10, Subctractionable.sub(10, 0));
+    }
+
+    @Test
+    public void shouldMultiplicationMult() {
+
+        Assert.assertEquals("проверка умножения",200, Multiplicationable.mult(20, 10));
+
+        Assert.assertEquals(1, Multiplicationable.mult(1, 1));
+
+        Assert.assertEquals(0, Multiplicationable.mult(10, 0));
+    }
+
+    @Test
+    public void shouldDivisionDiv() {
+
+        Assert.assertEquals("проверка деления",2, Divisionable.div(20, 10));
+
+        Assert.assertEquals(5, Divisionable.div(10, 2));
+
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void shouldCalculatorCanCalculateDivisionByZero() {
+        //       SecondProgram secondProgram = new SecondProgram(15, 0);
+        Divisionable.div(15, 0);
     }
 }
 
