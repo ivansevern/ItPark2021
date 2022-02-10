3cff9d91d3e873d1af65b1c9f329752ca9685317
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-02-10 23:13:31 +0300

    add  WeatherRunner for next refactoring and next future modify

---
+++ src/main/java/lesson22/MyWeather/WeatherRunnerMy.java
@@ -0,0 +1,60 @@
+package lesson22.MyWeather;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lesson22.MyWeather.dto.WeatherDto;
+import lesson22.MyWeather.exception.MandatoryParameterSkippedException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+//846cae20cf32ee6c4f3c1e7f1bb55b53
+//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+//https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric
+public class WeatherRunnerMy {
+
+    private static final ResourceBundle RESOURCES = ResourceBundle.getBundle("messages");
+    private static final String EXIT_PHRASE = RESOURCES.getString("app.exit");
+    private static final String APP_ID = "appId";
+    private static final String API_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric";
+
+    public static void main(String[] args) throws IOException {
+
+        final String appId = Optional.ofNullable(System.getProperty(APP_ID))
+                .orElseThrow(() -> new MandatoryParameterSkippedException(RESOURCES.getString("app.errorParameter")));
+        Scanner inpuScanner = new Scanner(System.in);
+        System.out.println(RESOURCES.getString("app.title"));
+        ObjectMapper objectMapper = new ObjectMapper();
+        while (inpuScanner.hasNextLine()) {
+            String city = inpuScanner.nextLine();
+            if (EXIT_PHRASE.equalsIgnoreCase(city)) {
+                return;
+            }
+//            String appId = Arrays.stream(args).findFirst()
+//                    .orElseThrow(() -> new MandatoryParameterSkippedException("Укажите при запуске обязательный параметр appId для подключения к API"));
+//            String appId = Optional.ofNullable(System.getenv(APP_ID))
+//                    .orElseThrow(() -> new MandatoryParameterSkippedException("Укажите при запуске обязательный параметр appId для подключения к API"));
+            String transformedCityName = getCityEr(city);
+            URL apiUrl = new URL(String.format(API_WEATHER_URL, transformedCityName, appId));
+            try (InputStream is = apiUrl.openStream()) {
+
+//                        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                WeatherDto weatherDto = objectMapper.readValue(is, WeatherDto.class);
+                System.out.printf(RESOURCES.getString("app.result"), city, weatherDto.getMain().getTemp());
+            }
+            System.out.println(RESOURCES.getString("app.nextPhrase"));
+        }
+    }
+
+
+    private static String getCityEr(String city) {
+        try {
+            return RESOURCES.getString(city);
+        } catch (Exception e) {
+            return "Samara";
+        }
+    }
+}
---
+++ src/main/java/lesson22/MyWeather/exception/MandatoryParameterSkippedException.java
@@ -0,0 +1,11 @@
+package lesson22.MyWeather.exception;
+
+/**
+ * Исключение, возникаемое на случай, когда обязательный параметр appId не указан при запуске программы
+ */
+public class MandatoryParameterSkippedException extends RuntimeException{
+
+    public MandatoryParameterSkippedException(String message) {
+        super(message);
+    }
+}
