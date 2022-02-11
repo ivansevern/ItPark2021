6651339b079add826cb7122c25c7c3c87ac86706
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-02-11 16:37:30 +0300

    Add and modify some result output weather from Teacher example

--- src/main/java/lesson22/MyWeather/WeatherRunnerMy.java
+++ src/main/java/lesson22/MyWeather/WeatherRunnerMy.java
@@ -3,7 +3,6 @@ package lesson22.MyWeather;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lesson22.MyWeather.dto.WeatherDto;
 import lesson22.MyWeather.exception.MandatoryParameterSkippedException;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -14,15 +13,16 @@ import java.util.Scanner;
 //846cae20cf32ee6c4f3c1e7f1bb55b53
 //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 //https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric
+//-DappId=846cae20cf32ee6c4f3c1e7f1bb55b53 -DtestParam=test
 public class WeatherRunnerMy {
 
     private static final ResourceBundle RESOURCES = ResourceBundle.getBundle("messages");
+    private static final String APP_CLOSE = RESOURCES.getString("app.close");
     private static final String EXIT_PHRASE = RESOURCES.getString("app.exit");
     private static final String APP_ID = "appId";
     private static final String API_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric";
 
     public static void main(String[] args) throws IOException {
-
         final String appId = Optional.ofNullable(System.getProperty(APP_ID))
                 .orElseThrow(() -> new MandatoryParameterSkippedException(RESOURCES.getString("app.errorParameter")));
         Scanner inpuScanner = new Scanner(System.in);
@@ -31,30 +31,31 @@ public class WeatherRunnerMy {
         while (inpuScanner.hasNextLine()) {
             String city = inpuScanner.nextLine();
             if (EXIT_PHRASE.equalsIgnoreCase(city)) {
+                System.out.println(RESOURCES.getString("app.close"));
                 return;
             }
-//            String appId = Arrays.stream(args).findFirst()
-//                    .orElseThrow(() -> new MandatoryParameterSkippedException("Укажите при запуске обязательный параметр appId для подключения к API"));
-//            String appId = Optional.ofNullable(System.getenv(APP_ID))
-//                    .orElseThrow(() -> new MandatoryParameterSkippedException("Укажите при запуске обязательный параметр appId для подключения к API"));
+
             String transformedCityName = getCityEr(city);
             URL apiUrl = new URL(String.format(API_WEATHER_URL, transformedCityName, appId));
             try (InputStream is = apiUrl.openStream()) {
-
-//                        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                 WeatherDto weatherDto = objectMapper.readValue(is, WeatherDto.class);
-                System.out.printf(RESOURCES.getString("app.result"), city, weatherDto.getMain().getTemp());
+                System.out.printf(RESOURCES.getString("app.result"),
+                        city, weatherDto.getMain().getTemp(),
+                        weatherDto.getMain().getPressure(),
+                        weatherDto.getWind().getSpeed(), weatherDto.getWind().getDeg(),
+                        weatherDto.getClouds().getAll(),
+                        weatherDto.getSys().getCountry()
+                );
             }
             System.out.println(RESOURCES.getString("app.nextPhrase"));
         }
     }
 
-
     private static String getCityEr(String city) {
         try {
             return RESOURCES.getString(city);
         } catch (Exception e) {
-            return "Samara";
+            return city;
         }
     }
 }
--- src/main/java/lesson22/MyWeather/dto/WeatherDto.java
+++ src/main/java/lesson22/MyWeather/dto/WeatherDto.java
@@ -11,9 +11,45 @@ public class WeatherDto {
     @JsonProperty("main")
     private MainDto main;
 
+    @JsonProperty("wind")
+    private WindDto wind;
+
+    @JsonProperty("clouds")
+    private CloudsDto clouds;
+
+    @JsonProperty("sys")
+    private SysDto sys;
+
+    @JsonProperty("deg")
+    private SysDto deg;
+
+
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class MainDto {
         private Double temp;
+        private Integer pressure;
     }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class WindDto {
+        private Double speed;
+        private String deg;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CloudsDto {
+        private String all;
+
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class SysDto {
+        private String country;
+    }
+
+
 }
