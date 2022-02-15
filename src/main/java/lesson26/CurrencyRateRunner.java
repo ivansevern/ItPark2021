86fe90e7cc70f2641861fc241c04a4fb5b5bbba8
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-02-15 12:48:58 +0300

    add beans

--- src/main/java/lesson26/CurrencyRateRunner.java
+++ src/main/java/lesson26/CurrencyRateRunner.java
@@ -23,51 +23,63 @@ java сделать программу с запросом по сокращен
 //<a href="https://www.cbr-xml-daily.ru/">Виджет курсов валют</a>
 //<a href="https://www.cbr-xml-daily.ru/">Курсы ЦБ РФ в XML и JSON, API</a>
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import java.io.IOException;
-import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 public class CurrencyRateRunner {
 
-    public static void main(String[] args) throws Exception {
-        try (InputStream inputStream = CurrencyRateRunner.class.getResourceAsStream("/cbr.xml")) {
-            Document xmlDocument = getXML(inputStream);
-            XPath xPath = XPathFactory.newInstance().newXPath();
-            String expression = "//Valute[@ID='R01020A']";
-            Node valuteNode = (Node) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODE);
-            System.out.println("ID узла: " + valuteNode.getAttributes().getNamedItem("ID").getNodeValue());
-            NodeList childNodes = valuteNode.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                Node node = childNodes.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE && "Value".equals(node.getNodeName())) {
-                    Element element = (Element) node;
-                    System.out.println("Курс валюты: " + element.getTextContent());
-                }
+        @Configuration
+        @ImportResource("classpath:beans.xml")
+        public class SpringBootXMLApplication implements CommandLineRunner {
+
+            @Autowired
+            private Currency currency;
+
+            @Override
+            public void run(String... strings) throws Exception {
+
             }
-            System.out.println("Содержимое тега Value: " + xPath.compile("//Valute[@ID='R01020A']/Value/text()").evaluate(xmlDocument, XPathConstants.STRING));
-
-            double avg = (Double) xPath.compile("sum(//Valute/Value) div count(//Valute/Value)").evaluate(xmlDocument, XPathConstants.NUMBER);
-            System.out.println("Среднее значение валют: " + avg);
         }
-    }
 
-    private static Document getXML(InputStream inputStream) throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = builderFactory.newDocumentBuilder();
-        return builder.parse(inputStream);
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(SpringBootXMLApplication.class, args);
+
+
     }
 }
+//        try (InputStream inputStream = CurrencyRateRunner.class.getResourceAsStream("/cbr.xml")) {
+//            Document xmlDocument = getXML(inputStream);
+//            XPath xPath = XPathFactory.newInstance().newXPath();
+//            String expression = "//Valute[@ID='R01020A']";
+//            Node valuteNode = (Node) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODE);
+//            System.out.println("ID узла: " + valuteNode.getAttributes().getNamedItem("ID").getNodeValue());
+//            NodeList childNodes = valuteNode.getChildNodes();
+//            for (int i = 0; i < childNodes.getLength(); i++) {
+//                Node node = childNodes.item(i);
+//                if (node.getNodeType() == Node.ELEMENT_NODE && "Value".equals(node.getNodeName())) {
+//                    Element element = (Element) node;
+//                    System.out.println("Курс валюты: " + element.getTextContent());
+//                }
+//            }
+//            System.out.println("Содержимое тега Value: " + xPath.compile("//Valute[@ID='R01020A']/Value/text()").evaluate(xmlDocument, XPathConstants.STRING));
+//
+//            double avg = (Double) xPath.compile("sum(//Valute/Value) div count(//Valute/Value)").evaluate(xmlDocument, XPathConstants.NUMBER);
+//            System.out.println("Среднее значение валют: " + avg);
+//        }
+//    }
+//
+//    private static Document getXML(InputStream inputStream) throws ParserConfigurationException, SAXException, IOException {
+//        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder builder = builderFactory.newDocumentBuilder();
+//        return builder.parse(inputStream);
+//    }
+//}
+
+
 //        Currency currency = new Currency();
 //        currency.setValuteId("R01010");
 //        currency.setNumCode("036");
@@ -107,4 +119,4 @@ public class CurrencyRateRunner {
 //}
 /*
 
-* */
+ * */
