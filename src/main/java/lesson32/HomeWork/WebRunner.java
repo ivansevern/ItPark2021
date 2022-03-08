2c74890ad4ddb003fa7886cb6dbfa67c006e2f01
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-03-08 21:36:47 +0300

    add lesson 32 and HW

---
+++ src/main/java/lesson32/HomeWork/WebRunner.java
@@ -0,0 +1,11 @@
+package lesson32.HomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(WebRunner.class, args);
+    }
+}
