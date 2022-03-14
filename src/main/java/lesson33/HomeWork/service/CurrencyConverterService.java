ea9eb849c39c1497b0ea33bf7a166e61cf54527d
Цуканов Иван Сергеевич <ivansevern@gmail.com>
2022-03-14 23:34:30 +0300

    add solution home work 33

--- src/main/java/lesson33/HomeWork/service/CurrencyConverterService.java
+++
@@ -1,6 +0,0 @@
-package lesson33.HomeWork.service;
-
-public interface CurrencyConverterService {
-
-    double result(double a, double b);
-}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,8 @@
-    //Объект с курсами 3-х валют
     const rates = {};
     //Элементы для отображения курса валют
     const elementUSD = document.querySelector('[data-value="USD"]');
     const elementEUR = document.querySelector('[data-value="EUR"]');
-    const elementGBP = document.querySelector('[data-value="GBP"]');
+
     //Элементы формы, ввод суммы, выбор валюты, поле с результатом
     const  input = document.querySelector('#input');
     const  result = document.querySelector('#result');
@@ -13,48 +12,37 @@
 
     //Функция получения курса валют и отображения их на странице
     async function getCurrencies() {
-        const response = await fetch('http://www.cbr-xml-daily.ru/daily_json.js');
+        const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
         const data = await response.json();
         const result = await data;
         console.log(result);
 
         rates.USD = result.Valute.USD;
         rates.EUR = result.Valute.EUR;
-        rates.GBP = result.Valute.GBP;
 
         console.log(rates);
 
         elementUSD.textContent = rates.USD.Value.toFixed(2);
         elementEUR.textContent = rates.EUR.Value.toFixed(2);
-        elementGBP.textContent = rates.GBP.Value.toFixed(2);
 
-        //Цвет для информера USD
+        //Цвет для визуализации изменения курса USD
         if (rates.USD.Value > rates.USD.Previous) {
             elementUSD.classList.add('top');
         } else {
             elementUSD.classList.add('bottom');
         }
 
-        //Цвет для информера EUR
+        //Цвет для визуализации изменения курса EUR
         if (rates.EUR.Value > rates.EUR.Previous) {
             elementEUR.classList.add('top');
         } else {
             elementUSD.classList.add('bottom');
         }
-
-        //Цвет для информера GBP
-        if (rates.GBP.Value > rates.GBP.Previous) {
-            elementGBP.classList.add('top');
-        } else {
-            elementGBP.classList.add('bottom');
-        }
     }
 
-    //Слушаем изменения в текстовом поле и в select
-    input.oninput = convertValue();
-    select.oninput = convertValue();
+    input.oninput = convertValue;
+    select.oninput = convertValue;
 
-    //Функция конвертации
     function convertValue() {
         result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
     }
